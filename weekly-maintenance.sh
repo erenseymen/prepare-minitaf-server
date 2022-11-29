3c8eefcede335229b841a6db9fa9889941208f95
Eren Seymen <eren.seymen@orioninc.com>
2022-11-30 01:40:25 +0300

    weekly-maintenance.sh is ready

--- weekly-maintenance.sh
+++ weekly-maintenance.sh
@@ -4,4 +4,21 @@
 # This script runs automatically by crontab on every Sunday at 00:00.
 # Author: Eren Seymen (eren.seymen@orioninc.com)
 
-touch /home/test-$(date +"%Y-%m-%d_%H-%M-%S").log
+{
+
+mkdir -p /root/logs
+
+
+echo Cleanup minitaf s useless logs
+rm -rf /home/*/a2-test/test/*/log
+rm -rf /home/*/a2-test/build/test-results
+
+echo Cleanup old SESM logs
+cd /home
+./cleanup_minitafs_old_sesm_logs.sh
+
+echo Compressing SESM logs
+gzip -r --fast /home/*/a2-test/build/ServiceTraces/
+
+
+} 2>&1 | tee /root/logs/weekly-maintenance-$(date +"%Y-%m-%d_%H-%M-%S").log
