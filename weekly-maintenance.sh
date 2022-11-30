d9bb21d8a0e77ddc072c13deb0857b622bc8bdd0
Eren Seymen <eren.seymen@orioninc.com>
2022-11-30 16:58:24 +0300

    Changed: From a2-test To a2-test*

--- weekly-maintenance.sh
+++ weekly-maintenance.sh
@@ -10,15 +10,15 @@ mkdir -p /root/logs
 
 
 echo "***** Cleanup minitaf's useless logs *****"
-rm -rf /home/*/a2-test/test/*/log
-rm -rf /home/*/a2-test/build/test-results
+rm -rf /home/*/a2-test*/test/*/log
+rm -rf /home/*/a2-test*/build/test-results
 
 echo "***** Cleanup old SESM logs *****"
 cd /home
 ./cleanup_minitafs_old_sesm_logs.sh
 
 echo "***** Compressing SESM logs *****"
-gzip -r --fast /home/*/a2-test/build/ServiceTraces/
+gzip -r --fast /home/*/a2-test*/build/ServiceTraces/
 
 
 } 2>&1 | tee /root/logs/weekly-maintenance-$(date +"%Y-%m-%d_%H-%M-%S").log
