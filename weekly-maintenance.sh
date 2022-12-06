cfde58abe62090b8aeffd7b7cb59ac4738d4ccee
Eren Seymen <eren.seymen@orioninc.com>
2022-12-06 13:16:02 +0300

    weekly-maintenance - report /home size

--- weekly-maintenance.sh
+++ weekly-maintenance.sh
@@ -4,10 +4,13 @@
 # This script runs automatically by crontab on every Sunday at 00:00.
 # Author: Eren Seymen (eren.seymen@orioninc.com)
 
-{
-
 mkdir -p /root/logs
 
+{
+
+echo "***** Size of the /home folder before maintenance (KB): *****"
+BEFORE_SIZE=$(du -s /home | awk '{print $1}')
+echo $BEFORE_SIZE
 
 echo "***** Cleanup minitaf's useless logs *****"
 rm -rf /home/*/a2-test*/test/*/log
@@ -20,5 +23,11 @@ cd /home
 echo "***** Compressing SESM logs *****"
 gzip -r --fast /home/*/a2-test*/build/ServiceTraces/
 
+echo "***** Size of the /home folder after maintenance (KB): *****"
+AFTER_SIZE=$(du -s /home | awk '{print $1}')
+echo $AFTER_SIZE
+
+echo "***** Disk space saved after maintenance (KB): *****"
+expr $BEFORE_SIZE - $AFTER_SIZE
 
 } 2>&1 | tee /root/logs/weekly-maintenance-$(date +"%Y-%m-%d_%H-%M-%S").log
