96528d1f093e8a568bf9101a1b95bfeb5178edbc
Eren Seymen <eren.seymen@orioninc.com>
2022-12-14 20:58:31 +0300

    cleanup_minitafs_old_sesm_logs - small fix

--- cleanup_minitafs_old_sesm_logs.sh
+++ cleanup_minitafs_old_sesm_logs.sh
@@ -3,7 +3,7 @@
 # This script recursively searches for "a2-test*/build/ServiceTraces" folders under the current directory. Under the found folders, the script will keep 10 newest SUCCESS and 10 newest FAILED logs for each TC and delete the others.
 # Author: Eren Seymen (eren.seymen@orioninc.com)
 
-find "$(pwd)" -type d -wholename "*a2-test*/build/ServiceTraces" | while read -r SERVICETRACES_DIR; do
+find "$(pwd)" -type d -wholename "*/a2-test*/build/ServiceTraces" | while read -r SERVICETRACES_DIR; do
     echo "Cleaning up $SERVICETRACES_DIR"
     ls -d "$SERVICETRACES_DIR"/*/*/ | while read -r TESTCASE_DIR; do
         ls -tp "$TESTCASE_DIR"SUCCESS_* 2> /dev/null | grep -v '/$' | tail -n +11 | xargs -d '\n' -r rm --
