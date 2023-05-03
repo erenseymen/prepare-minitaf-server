0ca81e30f4aae201b8ae583abc914a723e62a322
Eren Seymen <eren.seymen@orioninc.com>
2023-05-03 15:01:35 +0300

    jdk is 7z now because of github's file size limit

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # This script prepares minitaf server
 # Author: Eren Seymen (eren.seymen@orioninc.com)
 
-./main.sh jdk-8u371-linux-x64.tar.gz gradle-4.10.2-bin.zip
+./main.sh jdk-8u371-linux-x64.7z gradle-4.10.2-bin.zip
